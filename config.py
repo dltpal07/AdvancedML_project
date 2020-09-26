b58c2a1eb19ae7a062ea145ae0bdfa1f99c01919
dltpal07 <dltpal07@gmail.com>
2020-09-26 18:03:07 +0900

    Change Folders

---
+++ config.py
@@ -0,0 +1,8 @@
+PATH_CONFIG ={
+    'DATASET_PATH': '/content/drive/My Drive/Colab Notebooks/recommend_music_by_sentiment_analysis/dataset/',
+    'STOP_WORDS_FILE': 'stop_words_kr.txt',
+    'TRAIN_DATA_FILE': 'train_data.xlsx',
+    'TEST_DATA_FILE': 'test_data.xlsx',
+    'RESULT_PATH': '/content/drive/My Drive/Colab Notebooks/recommend_music_by_sentiment_analysis/result/',    
+
+    }
