621f677e2769b5d1682b18cf95e0dbe8aeeef1c1
dltpal07 <dltpal07@gmail.com>
2020-11-11 14:34:17 +0900

    Modify image path

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ PATH_CONFIG ={
     'TRAIN_DATA_FILE': 'train_data.xlsx',
     'TEST_DATA_FILE': 'test_data.xlsx',
     'RESULT_PATH': '/content/drive/My Drive/Colab Notebooks/recommend_music_by_sentiment_analysis/result/',
-    'TEMPLATE_FOLDER': '/content/drive/My Drive/Colab Notebooks/recommend_music_by_sentiment_analysis/web/templates/'
+    'TEMPLATE_FOLDER': '/content/drive/My Drive/Colab Notebooks/recommend_music_by_sentiment_analysis/web/templates/',
+    'STATIC_FOLDER': '/content/drive/My Drive/Colab Notebooks/recommend_music_by_sentiment_analysis/web/static'
 
     }
